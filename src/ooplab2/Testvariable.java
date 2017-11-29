9d9e42ced454900d51b8950ddf1a4d6d33239045
suvanan082 <suvanan1811@gmail.com>
2017-11-29 10:39:13 +0700

    Testvariable

--- src/ooplab2/Testvariable.java
+++ src/ooplab2/Testvariable.java
@@ -1,57 +1,65 @@
 package ooplab2;
 
-import java.util.Scanner;
-
 public class Testvariable {
     public static void main(String[] args) {
-//  integer
-        int i ;
-        i = 10;
-        System.out.println("i=" + i);
+//    Integer
+        int i;
+        i =10;
+        System.out.println("i="+i);
         int j = 20;
-        System.out.println("i+j=" + (i + j));
-        System.out.println("i*j=" + (i * j));
-        System.out.println("i/j=" + (i / j));
+        System.out.println("i+j="+j);
+        System.out.println("i+j="+(i+j));
+        System.out.println("i+j="+(i*j));
+        System.out.println("i+j="+(i/j));
 
-        //Double
+        //     Double
         double v = 50.5;
-        System.out.println("v=" + v);
-
+        System.out.println("v = "+v);
         i = (int) v;
         System.out.println(i);
         System.out.println(v);
 
-        //Charector
-        char c = 'x';
-        System.out.println(c);
+        //    Charector
+        char c ='x';
+        System.out .println(c);
 
-        //string
-        String s = "Hollo,wold";
+        // String
+        String s = "Hello, world";
         System.out.println(s);
-        String name ="I'PEAR";
-        System.out.println(s+" "+name);
-        String msg = s.concat(" "+name);
+        String name = "I'm ONE";
+        System.out.println(s+""+name);
+        String msg =s.concat(" "+name);
         System.out.println(msg);
         System.out.println(msg.toUpperCase());
-        int age = reader.resdLine();
-        System.out.println("You are"+age+"years old");
+        System.out.println(msg.toLowerCase());
+        System.out.println(msg.length());
+        System.out.println(msg.replace( 'o','g'));
 
-        //scanner
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("where are you from?:");
-        String address = scanner.nextLine();
-        System.out.println("you came From"+address);
-        System.out.print("What is your weight?:");
-        double weight = scanner.nextDouble();
-        System.out.println("Your weight: "+weight);
+        //Boolean
+        boolean b = true;
+        boolean bl= false;
+        System.out.println(b+""+bl);
 
+        //       Shortcut Operator
+        int number =10;
+        System.out.println(number);
+        number+=1; //number = number+1;
+        number-=5; //number = number-5;
+        int x = number++;
+
+        String n = "20";
+        //  int age = Integer.parseInt(n);
+        double age = Double.parseDouble(n);
+        System.out.println(age);
 
 
 
     }//main
 
-}//calss
 
+
+
+}//class
 
 
 
--- src/ooplab3/TestSwitich.java
+++ src/ooplab3/TestSwitich.java
@@ -3,17 +3,30 @@ package ooplab3;
 import java.util.Scanner;
 
 public class TestSwitich {
-    public static void main (String[]args)
-    Scanner scanner=new Scanner(System.in);
-    //switch-case
-    int select;
-    System.out.print("Please enter number 1-3:");
-    select =scanner.nextInt();
-    switch (select){
-        case 1: System.out.println("Your entered 1");break;
-        case 2: System.out.println("Your entered 2");break;
-        case 3: System.out.println("Your entered 3");break;
-        default: System.out.println("Number 1-3 only");
-    }//switch
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // switch-case
+        int select;
+        System.out.print("Please enter number 1-3: ");
+        select = scanner.nextInt();
+        switch (select) {
+            case 1:
+                System.out.println("Your entered 1");
+                break;
+            case 2:
+                System.out.println("Your entered 2");
+                break;
+            case 3:
+                System.out.println("Your entered 3");
+                break;
+            default:
+                System.out.println("Number 1-3 only.");
 
-}//main
+
+        }//switch
+
+
+    }//main
+
+
+}//class
