5ac088f7ce2b9a3edf294e750a2ea07fcd0637a5
suvanan082 <suvanan18111@hotmail.com>
2017-12-13 10:16:50 +0700

    TestArray

--- src/ooplab5/InputDataToArray.java
+++ src/ooplab5/InputDataToArray.java
@@ -5,21 +5,40 @@ package ooplab5;
 //4.average value in array
 
 public class InputDataToArray {
-    Private static final int MAX=5;
+    private static final int MAX = 5;
+    private static int number[] = new int[MAX];
+    public static void main(String[] args) {
 
-    public static void main (String[]args){
+        inputData(number,number.length);
+        showData(number,number.length);
+        sum(number,number.length);
 
-        int number[]= new
+    } //main
 
+    private static void sum(int[] number, int length) {
+        int total = 0;
+        for (int i=0;i<length;i++) {
+            total += number[i];
+        }
+        //System.out.println("Total in array is "+total);
+        System.out.println(total+"\n");
+        System.out.println("The avareage data in array: "+(total/length));
+    }
 
+    private static void showData(int[] number, int length) {
+        System.out.print("Data in array: ");
+        for (int i=0;i<length;i++){
+            System.out.print(number[i]+" ");
+        }
+        System.out.println();
+    } //showdata
 
-
-
-
-
-
-
-    }//main
-private static void inputData(int)
-
-}//class
+    private static void inputData(int[] number, int length) {
+        //user input data
+        Scanner scanner = new Scanner(System.in);
+        for (int i=0;i<length;i++) {
+            System.out.print("Enter an integer["+i+"]: ");
+            number[i] = scanner.nextInt();
+        }
+    } //inputdata
+} //class
