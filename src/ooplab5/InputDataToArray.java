18a380d8167d9983d0c4118e8d42c0acc21f15f1
suvanan082 <suvanan18111@hotmail.com>
2017-12-13 09:22:53 +0700

    TestArray

---
+++ src/ooplab5/InputDataToArray.java
@@ -0,0 +1,25 @@
+package ooplab5;
+//1.user input data to array
+//2.show data in arry
+//3.summation data in array
+//4.average value in array
+
+public class InputDataToArray {
+    Private static final int MAX=5;
+
+    public static void main (String[]args){
+
+        int number[]= new
+
+
+
+
+
+
+
+
+
+    }//main
+private static void inputData(int)
+
+}//class
