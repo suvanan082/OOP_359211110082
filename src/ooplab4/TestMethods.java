4b454d48e1e3d3aef241794a615957a7921654c3
suvanan082 <suvanan1811@gmail.com>
2018-01-31 08:52:24 +0700

    OverloadMethod

---
+++ src/ooplab4/TestMethods.java
@@ -0,0 +1,29 @@
+package ooplab4;
+
+public class TestMethods {
+    public static void A(){
+        System.out.println("Hello A");
+    }//A
+    public static void B(int x,int y,String s){
+        System.out.println(s);
+    }//B
+    public static int C(int x, int y){
+        return x+y;
+    }
+
+    public static void main(String[] args) {
+        //protected
+        //private
+        A();
+        int x=5,y=10;
+        B(x,y,"Hello");
+//            Manage return value
+//1.
+        int z = C(x,y);
+        System.out.println(z);
+//2.
+        System.out.println(C(x,y));
+
+    }//main
+}//class
+
