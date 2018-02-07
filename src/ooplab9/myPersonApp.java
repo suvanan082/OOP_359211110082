4bf18eac3ebc323673333ad120dbec72de69d6fc
suvanan082 <suvanan1811@gmail.com>
2018-02-07 17:36:42 +0700

    ooplob9

---
+++ src/ooplab9/myPersonApp.java
@@ -0,0 +1,19 @@
+package ooplab9;
+
+public class myPersonApp {
+    public static void main(String[] args) {
+        Person person = new Person("1111111111111",
+                "Puriwat Lertkrai",
+                new Address("109 M.2", "Nakorn",
+                        "80110"),
+                new Job("Teacher", 20000));
+
+        System.out.println(person.toString());
+
+        person.getJob().setSalary(30000);
+        System.out.println("Name: " + person.getName() +
+                " Salary: " + person.getJob().getSalary());
+
+    }   //main
+}
+
